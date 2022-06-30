86ee14a4983a6834da3f07c9e29dc3bb196edd7a
Rembrandt <rembrandt.oor@gmail.com>
2022-06-30 12:13:10 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype HTML>
+<html>
+
+<head>
+	<title>Hallo ik ben Charlotte</title>
+	<link rel="stylesheet" href="./style.css">
+</head>
+<body>
+	<h1>Hallo ik ben Charlotte</h1>	
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		<img src="./mouse.png" width="150">
+	</p>
+
+	<?php
+		$result = "";
+		
+		for ($i=0; $i < 10; $i++) { 
+			$result .= "x";
+		}
+		echo $result;
+
+
+	?>
+
+</body>
+</html>
